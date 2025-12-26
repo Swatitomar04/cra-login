54233d62e6b156b0255dbf60e001edcf13db1f4e
swatitomar-04 <stomar@idssoft.com>
2025-12-26 13:06:12 +0530

    fix root route redirect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route} from "react-router-dom";
+import { BrowserRouter, Routes, Route,Navigate} from "react-router-dom";
 
 import React from "react";
 import Login from "./components/login";
@@ -8,6 +8,7 @@ function App() {
   return ( 
       <BrowserRouter>
       <Routes>
+         <Route path="/" element={<Navigate to="/login" />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
       </Routes>
