e556f46946343fd55b7791ac195656fa334a0964
swatitomar-04 <stomar@idssoft.com>
2025-12-26 12:56:24 +0530

    Add register screen

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
+import { BrowserRouter, Routes, Route} from "react-router-dom";
+
 import React from "react";
 import Login from "./components/login";
+import Register from "./components/register";
 
 function App() {
-  return <Login />;
+  return ( 
+      <BrowserRouter>
+      <Routes>
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+   
+  )
 }
 
 export default App;
